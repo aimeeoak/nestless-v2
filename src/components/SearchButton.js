8daa514b079af51780b7364fd72937f32cc4cbcd
aimeeoak <aimeegarriock@gmail.com>
2022-02-28 11:56:21 -0800

    fixed pages and added react-router-dom

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,12 +1,11 @@
 import { React } from 'react';
 import { Button } from 'react-bootstrap';
 import "./SearchButton.css";
-import SearchPopup from "./SearchPopup.js";
 import searchbutton from "../images/search-button.png"
 
 export default function SearchButton() {
     return (
-    <Button className="button" onClick={SearchPopup}>
+    <Button className="button" onClick={}>
       <img src={searchbutton} className="search-button" alt="Search Button" />
     </Button>
     );
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Router, Switch, Route } from "react-router-dom";
+
+import About from "./components/AboutPage";
+import Search from "./components/SearchPage";
+import Results from "./components/ResultsPage";
+import Contact from "./components/ContactPage";
+
+
+export default class Routes extends Component {
+    render() {
+        return (
+            <Router history={history}>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/results" component={Results} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
+            </Router>
+        )
+    }
+}
