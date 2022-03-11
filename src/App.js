003fb0862621403b541b83fd6ed94a4b3ae21b8f
aimeeoak <aimeegarriock@gmail.com>
2022-03-11 15:28:35 -0800

    rendering fixed, ho

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import React from 'react';
 import 'react-bootstrap';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import Logo from './components/Logo';
 import ScanButton from './components/ScanButton';
 import SearchButton from './components/SearchButton';
 import Settings from './components/Settings';
-import About from "./AboutPage";
-import Search from "./SearchPage";
-import Results from "./ResultsPage";
-import Contact from "./ContactPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
           <div>
             <Settings />
             <Logo />
           </div>
-        </header>
-        <body>
           <div>
             <ScanButton />
+          </div>
+          <div>
             <SearchButton />
           </div>
-        </body>
       </div>
-      <Routes>
-        <Route path="/about" component={About} />
-        <Route path="/search" component={Search} />
-        <Route path="/results" component={Results} />
-        <Route path="/contact" component={Contact} />
-    </Routes>
     </Router>
   );
 }
