b68cb81ad9da6951898fed1615dd89e5313732f2
aimeeoak <aimeegarriock@gmail.com>
2022-03-11 15:14:21 -0800

    git version control fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
 import Logo from './components/Logo';
 import ScanButton from './components/ScanButton';
 import SearchButton from './components/SearchButton';
 import Settings from './components/Settings';
+import About from "./AboutPage";
+import Search from "./SearchPage";
+import Results from "./ResultsPage";
+import Contact from "./ContactPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <Settings />
-          <Logo />
-        </div>
-        <ScanButton />
-        <SearchButton />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Settings />
+            <Logo />
+          </div>
+        </header>
+        <body>
+          <div>
+            <ScanButton />
+            <SearchButton />
+          </div>
+        </body>
+      </div>
+      <Routes>
+        <Route path="/about" component={About} />
+        <Route path="/search" component={Search} />
+        <Route path="/results" component={Results} />
+        <Route path="/contact" component={Contact} />
+    </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,12 +1,19 @@
 import { React } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import "./SearchButton.css";
 import searchbutton from "../images/search-button.png"
 
+
 export default function SearchButton() {
-    return (
-    <Button className="button" onClick={}>
-      <img src={searchbutton} className="search-button" alt="Search Button" />
-    </Button>
-    );
+  let navigate = useNavigate();
+
+  const SearchPage = () => {
+    navigate.push('/search')
   }
+  return (
+  <Button className="button" onClick={SearchPage}>
+    <img src={searchbutton} className="search-button" alt="Search Button" />
+  </Button>
+  );
+};
