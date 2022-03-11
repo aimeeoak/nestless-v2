82f954f54671f8fde84a694acb6d6053d99e2c76
aimeeoak <aimeegarriock@gmail.com>
2022-03-11 15:29:38 -0800

    prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import 'react-bootstrap';
+import React from "react";
+import "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
-import './App.css';
-import Logo from './components/Logo';
-import ScanButton from './components/ScanButton';
-import SearchButton from './components/SearchButton';
-import Settings from './components/Settings';
+import "./App.css";
+import Logo from "./components/Logo";
+import ScanButton from "./components/ScanButton";
+import SearchButton from "./components/SearchButton";
+import Settings from "./components/Settings";
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <div>
-            <Settings />
-            <Logo />
-          </div>
-          <div>
-            <ScanButton />
-          </div>
-          <div>
-            <SearchButton />
-          </div>
+        <div>
+          <Settings />
+          <Logo />
+        </div>
+        <div>
+          <ScanButton />
+        </div>
+        <div>
+          <SearchButton />
+        </div>
       </div>
     </Router>
   );
 }
 
 export default App;
-
-
